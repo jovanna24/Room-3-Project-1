86dfc26b7234232a10526f05e2e3ef2278669c55
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-04-09 20:50:52 -0700

    edited storage functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 function saveOrgSearches(organizations){
     localStorage.setItem('organizations', JSON.stringify(organizations))
-} 
+}  
+
+function readSearchesFromStorage() {
+    let organizations = JSON.parse(localStorage.getItem('organizations')); 
+    if(!organizations){
+        organizations= [];
+    }
+    return organizations;
+}
+
 
