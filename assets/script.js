984ca7a1d6e40ce2d4d2c322113ab1178e6ef574
aeschalna <aeschalna@gmail.com>
2024-04-11 20:50:36 -0700

    day #3 commit

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
 //Add variables (global scope)
+const selection = document.querySelectorAll('select');
+const zipCode = document.getElementById('location');
 
 //Connect buttons to html counterparts
 const openModalButton = document.getElementById('open-modal');
 const closeButton = document.getElementById('modal-close');
 const modalForm = document.getElementById('modal');
+const submitSearch = document.getElementById('submit');
 
 // Functions to open/close modal 
 function openModal() {
@@ -14,9 +17,30 @@ function closeModal() {
     modalForm.classList.remove('is-active');
 }
 
-// Add event listener to openModalBtn
+// On form submit, save search values for APIs
+const SAVE_KEY = "search";
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const searchThis = {
+        category: selection,
+        location: zipCode.trim(),
+    }
+    setSearchInLocalStorage(searchThis);
+}
+
+// Function to set up localStorage
+function setSearchInLocalStorage(search) {
+    localStorage.setItem(SAVE_KEY, JSON.stringify(search))
+}
+// Retrieve search object
+function getSearchFromLocalStorage() {
+    const searchString = localStorage.getItem(SAVE_KEY);
+    return searchString ? JSON.parse(searchString) : [];
+}
+
+// Add event listeners
 openModalButton.addEventListener('click', openModal);
 closeButton.addEventListener('click', closeModal);
-
-
-// Function to create user interest object
+submitSearch.addEventListener('submit', handleFormSubmit);
