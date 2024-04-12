3b4c6bb1fa0a1979bb0ae26f9202d953f33c12d5
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-04-11 19:32:34 -0700

    fixed API search, still need to add proper parameters from results array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+const resultContentEl = document.querySelector('#result-content');
+
 function saveOrgSearches(organizations){
     localStorage.setItem('organizations', JSON.stringify(organizations))
 }  
@@ -10,16 +12,46 @@ function readSearchesFromStorage() {
     return organizations;
 }
 
-fetch ('https://projects.propublica.org/nonprofits/api/v2', {
-    method: 'GET', 
-    headers: {
-        'Content-type': 'application/json'
-    }, 
-    body: JSON.stringify({
-        name: 'Organization 1'
-    })
-}).then(res => {
-    return res.json()
-})
-.then(data => console.log(data)) 
-.catch(error => console.log('ERROR'))
+
+function searchApi(organization) {
+    const publicaAPI = `https://projects.propublica.org/nonprofits/api/v2`;
+
+    fetch(publicaAPI) 
+        .then(function(response){
+            if (!response.ok) {
+                throw new Error('Server response was not ok.');
+            } 
+            return response.json(); 
+        })
+        .then(function(organization){
+            console.log(organization); 
+            displayOrganization(organization); 
+        }) 
+        .catch(function(error){
+            console.log('Error fetching data', error); 
+            resultContentEl.innerHTML = '<h3> Error fetching data, please try new search. </h3>';
+
+        })
+ }
+
+function displayOrganization(organization) {
+    if(!organization.name) {
+        console.log('No results found.'); 
+        resultContentEl.innerHTML = '<h3> Error fetching data, please try new search. </h3>';
+    } else {
+        resultContentEl.innerHTML= ''; 
+
+        const resultCard = document.createElement('div'); 
+
+        const resultBody = document.createElement('div'); 
+        resultCard.append(resultBody); 
+
+        //Need to add display parameters
+
+        resultContentEl.append(resultCard);
+
+    }
+}
+
+
+
