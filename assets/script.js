082ff6f81cdc55fa27a78bc45a2c1b71a275160a
aeschalna <aeschalna@gmail.com>
2024-04-15 18:53:31 -0700

    combined codebase

--- assets/script.js
+++ assets/script.js
@@ -106,3 +106,53 @@ function initMap(coords) {
     }).addTo(map);
     marker = L.marker(coords).addTo(map);
 }
+
+// Function to save selected organizations to local storage
+function savedOrgs
+
+// Function to search nonprofits using value given per category
+// Limit results to 10 per page
+function searchAPI(organization) {
+  const publicaAPI = `https://projects.propublica.org/nonprofits/api/v2`;
+
+  fetch(publicaAPI) 
+      .then(function(response){
+          if (!response.ok) {
+              throw new Error('Server response was not ok.');
+          } 
+          return response.json(); 
+      })
+      .then(function(organization){
+          console.log(organization); 
+          displayOrganization(organization); 
+      }) 
+      .catch(function(error){
+          console.log('Error fetching data', error); 
+          resultContentEl.innerHTML = '<h3> Error fetching data, please try new search. </h3>';
+
+      })
+}
+
+function displayOrganization(organization) {
+  if(!organization.name) {
+      console.log('No results found.'); 
+      resultContentEl.innerHTML = '<h3> Error fetching data, please try new search. </h3>';
+  } else {
+      resultContentEl.innerHTML= ''; 
+
+      const resultCard = document.createElement('div'); 
+
+      const resultBody = document.createElement('div'); 
+      resultCard.append(resultBody); 
+
+      const bodyContentEl = document.createElement('p');
+      bodyContentEl.innerHTML +=
+      `<strong>Organization Name: </strong> ${organization.sub_name}<br/>` +
+      `<strong>Organization Address: </strong> ${organization.address}<br/>` +
+      `<strong>Organization City: </strong> ${organization.city}<br/>` +
+
+      resultContentEl.append(resultCard);
+
+  }
+}
+}
